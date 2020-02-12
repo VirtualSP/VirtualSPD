99258480ade3c9d6e6b14ff9aa64185f0b85bd6a
VirtualSP <masami_amano@hotmail.com>
2020-02-13 07:55:20 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-var CACHE_NAME = 'VSPcache1.8.2.1';
+var CACHE_NAME = 'VSPcache1.8.2.2';
 var urlsToCache = [
-    'https://virtualsp.github.io/VirtualSPD/',
-    'https://virtualsp.github.io/VirtualSPD/js/app66.js',
-    'https://virtualsp.github.io/VirtualSPD/js/three.min.js',
-     'https://virtualsp.github.io/VirtualSPD/js/musicmetadata.js'
+    '/',
+    '/js/app66.js',
+    '/js/three.min.js',
+    '/js/musicmetadata.js'
 ];
 
 self.addEventListener('install', function(event) {
